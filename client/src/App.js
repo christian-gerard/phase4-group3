af998278c6cf9b173241dc66caac990455ff0c6c
pwangy <blipsandclicks@gmail.com>
2024-04-09 11:55:26 -0700

    reset html and css; inc starter styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
+const App = () => {
+	return (
+		<div className='main'>
+			<h1>Phase 4 - Group 3 &#128007;</h1><hr/>
+      <ul>
+        <li>Yume</li>
+        <li>Somni</li>
+        <li>Hypno</li>
+        <li>Nocturne</li>
+        <li>Lucidity</li>
+        <li>Rabbit Rabbit</li>
+        <li>Luna Something</li>
+        <li>Aurora Something</li>
+        <li>Moonrise Something</li>
+      </ul>
+		</div>
+)}
 
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Phase4-Group3</h1>
-    </div>
-  );
-}
-
-export default App;
+export default App
