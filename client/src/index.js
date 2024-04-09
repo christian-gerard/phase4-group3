e308efcb463c20eb85e725d4866c3d2b27fbc916
pwangy <blipsandclicks@gmail.com>
2024-04-09 15:26:15 -0700

    inc react-router-dom

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import router from './utils/routes'
+import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
 import './style.scss'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement)
+
+root.render(
+    <RouterProvider router={router} />
+)
