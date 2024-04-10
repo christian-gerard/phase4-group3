b35d93205cdbb6457162f20603235b0fd10863ef
pwangy <blipsandclicks@gmail.com>
2024-04-10 12:51:03 -0700

    possible titles on one line

--- client/src/App.js
+++ client/src/App.js
@@ -7,26 +7,18 @@ const App = () => {
 		<div className='main'>
 			<h1>Phase 4 - Group 3 &#128007;</h1><hr/>
       <ul>
-        <li>Yume</li>
-        <li>Somni</li>
-        <li>Hypno</li>
-        <li>Nocturne</li>
-        <li>Lucidity</li>
-        <li>Rabbit Rabbit</li>
-        <li>Luna Something</li>
-        <li>Aurora Something</li>
-        <li>Moonrise Something</li>
+        <li>Yume</li> | <li>Somni</li> | <li>Hypno</li> | <li>Nocturne</li> | <li>Lucidity</li> | <li>Rabbit Rabbit</li> | <li>Luna Something</li> |  <li>Aurora Something</li> | <li>Moonrise Something</li>
       </ul>
         <h2>Test Colors</h2>
       <section>
-        <div id='a' className='color-test'></div>
-        <div id='b' className='color-test'></div>
-        <div id='c' className='color-test'></div>
-        <div id='d' className='color-test'></div>
-        <div id='e' className='color-test'></div>
-        <div id='f' className='color-test'></div>
-        <div id='g' className='color-test'></div>
-        <div id='h' className='color-test'></div>
+        <div id='a' className='color-test' />
+        <div id='b' className='color-test' />
+        <div id='c' className='color-test' />
+        <div id='d' className='color-test' />
+        <div id='e' className='color-test' />
+        <div id='f' className='color-test' />
+        <div id='g' className='color-test' />
+        <div id='h' className='color-test' />
       </section>
       <section className='tester'>
         <Nav />
