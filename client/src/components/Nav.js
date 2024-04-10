a5546b5c5fdea4a34cdebaedd4e22133fb4d847b
pwangy <blipsandclicks@gmail.com>
2024-04-10 15:53:51 -0700

    tidy up nav

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -6,7 +6,6 @@ const Nav = () => {
             <NavLink id='link' to='/' className=''>Home</NavLink>
             <NavLink id='link' to='/new' className=''>New Entry</NavLink>
             <NavLink id='link' to='/view' className=''>View Journal</NavLink>
-            <NavLink id='link' to='/reg' className=''>Registration</NavLink>
             <NavLink id='link' to='/' className=''>Logout</NavLink>
         </nav>
     )
