cb8b3663a35148d9a7eb39721162eb4e551e858c
pwangy <blipsandclicks@gmail.com>
2024-04-11 10:10:57 -0700

    update title everywhere

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,11 @@ import Footer from './components/Footer'
 const App = () => {
 	return (
 		<div className='main'>
-			<h1>Phase 4 - Group 3 &#128007;</h1><hr/>
-      <ul>
-        <li>Yume</li> | <li>Somni</li> | <li>Hypno</li> | <li>Nocturne</li> | <li>Lucidity</li> | <li>Rabbit Rabbit</li> | <li>Luna Something</li> |  <li>Aurora Something</li> | <li>Moonrise Something</li>
-      </ul>
+      <section className='tester'>
+        <Nav />
+        <Outlet />
+        <Footer />
+      </section>
         <h2>Test Colors</h2>
       <section>
         <div id='a' className='color-test' />
@@ -19,11 +20,6 @@ const App = () => {
         <div id='f' className='color-test' />
         <div id='g' className='color-test' />
         <div id='h' className='color-test' />
-      </section>
-      <section className='tester'>
-        <Nav />
-        <Outlet />
-        <Footer />
       </section>
 		</div>
 )}
