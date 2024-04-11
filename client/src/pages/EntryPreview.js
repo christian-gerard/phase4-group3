8122209c32957c964f6ddc9ee94a46608a6b7091
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-04-11 16:20:19 -0600

    Adds pip fixes and entrypreview

---
+++ client/src/pages/EntryPreview.js
@@ -0,0 +1,13 @@
+
+function EntryPreview() {
+
+
+    return (
+        <>
+            <h5>Date - Title If applicable</h5>
+            <p>Body Preview ~100 Char</p>
+        </>
+    )
+}
+
+export default EntryPreview
