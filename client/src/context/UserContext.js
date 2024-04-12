c05ef5004978f6d8f0519bf7903dd893b0cb07e2
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-04-12 14:34:28 -0600

    Sign in Users, Create Users, and update user context

--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -1,39 +1,18 @@
-import { createContext, useState, useEffect } from 'react'
+import { createContext, useState } from 'react'
 
 export const UserContext = createContext()
 
 const UserProvider = ({ children }) => {
-    const url = 'http://localhost:5555/api/v1'
     const [user, setUser] = useState(null)
 
-    useEffect(() => {
-        (async () => {
-            try {
-                const res = await fetch(`${url}/login`)
-                const data = await res.json()
-                setUser(data)
-            } catch (err) {
-                throw new Error('Not found')
-            }
-        })()
-    }, [])
 
     const login = (user) => {
         setUser(user)
     }
 
-    const logout = (user) => {
+    const logout = () => {
         setUser(null)
     }
-
-    // const handleNewUser = async () => {
-    //     try {
-
-    //     } catch (err) {
-            
-    //     }
-        
-    // }
 
     return (
         <UserContext.Provider value={{ user, login, logout }}>
