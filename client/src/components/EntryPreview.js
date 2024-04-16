7a07e9fef8a64e97b3842bbed01fe6e88655839d
pwangy <blipsandclicks@gmail.com>
2024-04-16 15:39:57 -0700

    view all entries

--- client/src/components/EntryPreview.js
+++ client/src/components/EntryPreview.js
@@ -1,11 +1,12 @@
 import { NavLink } from 'react-router-dom'
 
 function EntryPreview({id, title, body, date, category}) {
-
     return (
-        <NavLink to={`/view/${id}`}>
-            <p className='entry-link'>{date} | {title}</p>
-            <p className='entry-link long-desc'>{body}</p>
+        <NavLink className='entry-link' to={`/view/${id}`}>
+            <p>
+                <span className='entry-short'>{date} | {title} </span>
+                {body}
+            </p>
         </NavLink>
 )}
 
