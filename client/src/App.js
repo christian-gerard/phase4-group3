1cba643c56d35009ed4d4a8a1a9e2b1e26638837
pwangy <blipsandclicks@gmail.com>
2024-04-16 20:12:58 -0700

    code cleaning

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,13 @@
 import { Outlet } from 'react-router-dom'
 import Nav from './components/Nav'
 import Footer from './components/Footer'
-import { useState, useEffect } from 'react'
-import toast from 'react-hot-toast'
 
 const App = () => {
-
   return (
 		<main>
-      <Nav />
-      <Outlet context={{ }}/>
-      <Footer />
+         <Nav />
+         <Outlet context={{ }} />
+         <Footer />
 		</main>
 )}
 
--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -4,36 +4,34 @@ import toast from 'react-hot-toast'
 export const UserContext = createContext()
 
 const UserProvider = ({ children }) => {
-    const [user, setUser] = useState(null)
+	const [user, setUser] = useState(null)
 
-    const login = (user) => {
-        setUser(user)
-    }
+	const login = (user) => {
+		setUser(user)
+	}
 
-    const logout = () => {
-        try {
-            fetch("/logout", {method: "DELETE"})
-            .then(resp => {
-                if (resp.status === 204) {
-                    setUser(null)
-                    toast.success('Entry Deleted')
-                } else {
-                    toast.error('Deletion failed...')
-                }
-            })
-        }
-        catch(err) {
-            throw err
+	const logout = () => {
+		try {
+			fetch('/logout', { method: 'DELETE' }).then((resp) => {
+				if (resp.status === 204) {
+					setUser(null)
+					toast.success('Entry Deleted')
+				} else {
+					toast.error('Could not delete!')
+				}
+			})
+		} catch (err) {
+			throw err
     }}
 
-    const updateEntries = (updatedEntries) => {
-        setUser({...user, entries: updatedEntries})
-    }
+	const updateEntries = (updatedEntries) => {
+		setUser({ ...user, entries: updatedEntries })
+	}
 
-    return (
-        <UserContext.Provider value={{ user, login, logout, updateEntries }}>
-            {children}
-        </UserContext.Provider>
+	return (
+		<UserContext.Provider value={{ user, login, logout, updateEntries }}>
+			{children}
+		</UserContext.Provider>
 )}
 
 export default UserProvider
