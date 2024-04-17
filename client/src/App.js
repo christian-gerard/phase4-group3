fd94c3dde5ef73920d59efd3ee885172a226fb84
pwangy <blipsandclicks@gmail.com>
2024-04-17 12:37:23 -0700

    cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import { Outlet, useNavigate } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import { UserContext } from './context/UserContext'
 import Nav from './components/Nav'
@@ -7,18 +7,13 @@ import Footer from './components/Footer'
 
 const App = () => {
   const { user } = useContext(UserContext)
-  const redirect = useNavigate()
 
   return (
 		<main>
-         <Nav />
-         <Toaster position="top-center" containerClassName="toaster-style" />
-         {/* {user ? ( */}
-              <Outlet context={{ user }} />
-         {/* ) : (  */}
-          {/* redirect('/') */}
-         {/* )} */}
-         <Footer />
+        <Nav />
+        <Toaster position="top-center" containerClassName="toaster-style" />
+        <Outlet context={{ user }} />
+        <Footer />
 		</main>
 )}
 
