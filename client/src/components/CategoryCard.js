fe6f1dea291f9edd470109384c669bed427d162e
Stephanie Bertram <steph.a.bertram@gmail.com>
2024-04-17 21:11:18 -0700

    cleanup

--- client/src/components/CategoryCard.js
+++ client/src/components/CategoryCard.js
@@ -11,19 +11,6 @@ const CategoryCard = ({name, description, icon}) => {
                 <p id='category-descr'>{description}</p>
             </div>
         </div>
-
-
-        // <div id='category-container'>
-        //     <div id='category-icon'> 
-        //         <img src={icon} alt={name}/>
-        //     </div>
-        //     <div id='category-name'> 
-        //         <h3>{name}</h3>
-        //     </div>
-        //     <div id='category-descr'>
-        //         <p>{description}</p>
-        //     </div>
-        // </div>
 )}
 
 export default CategoryCard
